a8ee7687f655c251187248b3593b29e1f2e59122
Limengtao <1104234512@qq.com>
2019-01-26 18:02:13 +0800

    整改

--- router/newbook.route.js
+++ router/newbook.route.js
@@ -4,7 +4,15 @@ const pool = require("../pool");
 
 
 router.get("/",(req,res)=>{
-    
+    var sql = `select * from new_book`;
+    pool.query(sql,[],(err,result)=>{
+        if(err) console.log(err);
+        res.writeHead(200,{
+            "Access-Countrol-Allow-Origin":"*"
+        });
+        res.write(JSON.stringify(result))
+        res.end();
+    })
 })
 
 module.exports = router;
